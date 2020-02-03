38c06f89a53624906988341c5c3950bbda30459b
Oleh Novosad <oleh.novosad@gmail.com>
2020-02-03 13:29:22 -0500

    In progress

--- test/lib/list/test_list.c
+++ test/lib/list/test_list.c
@@ -89,32 +89,32 @@ void test_list1(void)
 
 void test_list_fast_ma(void)
 {
-    // Mem mem     = mem_create(2048);
+    Mem mem     = mem_create(2048);
 
-    // Neu n1 = (Neu) mem_alloc(mem, sizeof(struct neu));
-    // Neu n2 = (Neu) mem_alloc(mem, sizeof(struct neu));
-    // Neu n3 = (Neu) mem_alloc(mem, sizeof(struct neu));
-    // Neu n4 = (Neu) mem_alloc(mem, sizeof(struct neu));
-    // Neu n5 = (Neu) mem_alloc(mem, sizeof(struct neu));
+    Neu n1 = (Neu) mem_alloc(mem, sizeof(struct neu));
+    Neu n2 = (Neu) mem_alloc(mem, sizeof(struct neu));
+    Neu n3 = (Neu) mem_alloc(mem, sizeof(struct neu));
+    Neu n4 = (Neu) mem_alloc(mem, sizeof(struct neu));
+    Neu n5 = (Neu) mem_alloc(mem, sizeof(struct neu));
 
-    // List list = list_create(mem, 10);
+    List list = list_create(mem, 10);
 
-    // L_Node node1 = list_append(list, n1);
-    // L_Node node2 = list_append(list, n2);
-    // L_Node node3 = list_append(list, n3);
+    L_Node node1 = list_append(list, n1);
+    L_Node node2 = list_append(list, n2);
+    L_Node node3 = list_append(list, n3);
 
-    // TEST_ASSERT(list_count(list) == 3);
-    // TEST_ASSERT(list->first->value == n1);
-    // TEST_ASSERT(list->last->value == n3);
-    // TEST_ASSERT(list->first->next->value == n2);
+    TEST_ASSERT(list_count(list) == 3);
+    TEST_ASSERT(list->first->value == n1);
+    TEST_ASSERT(list->last->value == n3);
+    TEST_ASSERT(list->first->next->value == n2);
 
-    // L_Node node4 = list_prepend(list, n4);
-    // TEST_ASSERT(list_count(list) == 4);
-    // TEST_ASSERT(list->first->value == n4);
+    L_Node node4 = list_prepend(list, n4);
+    TEST_ASSERT(list_count(list) == 4);
+    TEST_ASSERT(list->first->value == n4);
 
-    // list_remove(list, node2);
+    list_remove(list, node2);
 
-    // TEST_ASSERT(list->count == 3);
+    TEST_ASSERT(list->count == 3);
 
-    // mem_destroy(mem);
+    mem_destroy(mem);
 }
