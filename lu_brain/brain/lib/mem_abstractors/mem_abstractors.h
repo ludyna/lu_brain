b826b2ddaec419882ddabc17589bf93ac335f387
Oleh Novosad <oleh.novosad@gmail.com>
2020-03-26 15:38:01 -0400

    In progressI

--- lu_brain/brain/lib/mem_abstractors/mem_abstractors.h
+++ lu_brain/brain/lib/mem_abstractors/mem_abstractors.h
@@ -19,10 +19,10 @@
 
 	struct mem {
 
-		lu_p_byte 	(*alloc)(Mem, lu_size size, const char* file, int line);
-		lu_p_byte 	(*realloc)(Mem, lu_p_byte, lu_size, const char* file, int line);
-		void 		(*free)(Mem, lu_p_byte, const char* file, int line);
-		void 		(*destroy)(Mem, Mem, const char* file, int line);
+		lu_p_byte 	(*alloc)(Mem, lu_size size, const char* func, const char* file, int line);
+		lu_p_byte 	(*realloc)(Mem, lu_p_byte, lu_size, const char* func, const char* file, int line);
+		void 		(*free)(Mem, lu_p_byte, const char* func, const char* file, int line);
+		void 		(*destroy)(Mem, Mem, const char* func, const char* file, int line);
 
 		Mem_Table 	(*table_create)(
 			Mem 		mem, 
@@ -30,21 +30,22 @@
 	 		lu_size 	table_size_in_records, 
 	 		lu_value 	percent,
 	 		lu_flags 	flags,
+	 		const char* func, 
 	 		const char* file,
 	 		int line
 	 	);
 	};
 
-	#define mem_alloc(mem, size) mem->alloc(mem, size, __FILE__, __LINE__)
-	#define mem_realloc(mem, p, size) mem->realloc(mem, p, size, __FILE__, __LINE__)
-	#define mem_free(mem, p) mem->free(mem, p, __FILE__, __LINE__)
-	#define mem_destroy(mem, parent_mem) mem->destroy(mem, parent_mem, __FILE__, __LINE__)
-	#define mem_table_create(mem, r_size, t_size, p, f) mem->table_create(mem, r_size, t_size, p, f, __FILE__, __LINE__)
+	#define mem_alloc(mem, size) mem->alloc(mem, size, __func__, __FILE__, __LINE__)
+	#define mem_realloc(mem, p, size) mem->realloc(mem, p, size, __func__, __FILE__, __LINE__)
+	#define mem_free(mem, p) mem->free(mem, p, __func__, __FILE__, __LINE__)
+	#define mem_destroy(mem, parent_mem) mem->destroy(mem, parent_mem, __func__, __FILE__, __LINE__)
+	#define mem_table_create(mem, r_size, t_size, p, f) mem->table_create(mem, r_size, t_size, p, f, __func__, __FILE__, __LINE__)
 
-	#define mem_temp_alloc(size) g_mem_temp->alloc(g_mem_temp, size, __FILE__, __LINE__)
-	#define mem_temp_realloc(p, size) g_mem_temp->realloc(g_mem_temp, p, size, __FILE__, __LINE__)
-	#define mem_temp_free(p) g_mem_temp->free(g_mem_temp, p, __FILE__, __LINE__)
-	#define mem_temp_table_create(r_size, t_size, p, f) g_mem_temp->table_create(g_mem_temp, r_size, t_size, p, f, __FILE__, __LINE__)
+	#define mem_temp_alloc(size) g_mem_temp->alloc(g_mem_temp, size, __func__, __FILE__, __LINE__)
+	#define mem_temp_realloc(p, size) g_mem_temp->realloc(g_mem_temp, p, size, __func__, __FILE__, __LINE__)
+	#define mem_temp_free(p) g_mem_temp->free(g_mem_temp, p, __func__, __FILE__, __LINE__)
+	#define mem_temp_table_create(r_size, t_size, p, f) g_mem_temp->table_create(g_mem_temp, r_size, t_size, p, f, __func__, __FILE__, __LINE__)
 
 	Mem mem_create();
 
@@ -56,11 +57,11 @@
 	};
 
 	struct mem_table {
-		void 		(*realloc)(Mem_Table, lu_size new_size_in_bytes, lu_flags flags, const char* file, int line);
-	 	void 		(*destroy)(Mem_Table, Mem, const char* file, int line);
+		void 		(*realloc)(Mem_Table, lu_size new_size_in_bytes, lu_flags flags, const char* func, const char* file, int line);
+	 	void 		(*destroy)(Mem_Table, Mem, const char* func, const char* file, int line);
 
-		lu_p_byte 	(*record_alloc)(Mem_Table, const char* file, int line);
-		void 		(*record_free)(Mem_Table, lu_p_byte record, const char* file, int line);
+		lu_p_byte 	(*record_alloc)(Mem_Table, const char* func, const char* file, int line);
+		void 		(*record_free)(Mem_Table, lu_p_byte record, const char* func, const char* file, int line);
 
 		lu_size 		record_size_in_bytes;
 		lu_size			table_size_in_records;
@@ -75,11 +76,11 @@
 		lu_size 		free_count;
 	};
 
-	#define mem_table_realloc(mt, n_size, f) mt->realloc(mem, n_size, f, __FILE__, __LINE__)
-	#define mem_table_destroy(mt, parent_mem) mt->destroy(mt, parent_mem, __FILE__, __LINE__)
+	#define mem_table_realloc(mt, n_size, f) mt->realloc(mem, n_size, f, __func__, __FILE__, __LINE__)
+	#define mem_table_destroy(mt, parent_mem) mt->destroy(mt, parent_mem, __func__, __FILE__, __LINE__)
 
-	#define mem_record_alloc(mt) mt->record_alloc(mt, __FILE__, __LINE__)
-	#define mem_record_free(mt, p) mt->record_free(mt, p, __FILE__, __LINE__)
+	#define mem_record_alloc(mt) mt->record_alloc(mt, __func__, __FILE__, __LINE__)
+	#define mem_record_free(mt, p) mt->record_free(mt, p, __func__, __FILE__, __LINE__)
 
 ///////////////////////////////////////////////////////////////////////////////
 // Mem_Perm
@@ -125,6 +126,7 @@
 		lu_size 			table_size_in_records, 
 		lu_value 			percent,
 		lu_flags 			flags,
+		const char* 		func, 
 		const char* 		file,
 		int 				line
 	);
