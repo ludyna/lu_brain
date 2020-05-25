baaaffd9afd88c359b0eaed30cf913213c855906
Oleh Novosad <oleh.novosad@gmail.com>
2020-05-24 20:04:14 -0400

    In progress

--- test/s/test_s_col.c
+++ test/s/test_s_col.c
@@ -10,20 +10,42 @@ Lu_Rec 				rec_1;
 
 Lu_Brain_Opts 		brain_opts;
 Lu_Rec_Opts 		rec_opts_1;
+Lu_Rec_Opts 		rec_opts_2;
 
-lu_value			data_0[] 		= { 0, 0, 1, 1 };
+lu_value			data_0[] 		= { 0 }; 
+lu_value			data_1[] 		= { 1.0 };
+lu_value			data_3[] 		= { 3.0 };
+lu_value			data_5[] 		= { 5.0 };
+lu_value			data_10[] 		= { 10.0 };
+lu_value			data_999[] 		= { 999.0 };
+lu_value			data_m1[] 		= { -1.0 };
 
 Mem 				mem;
 S_Mem				s_mem;
 S_Rec 				s_rec;
 S_Cell				s_cell;
-S_Col 				s_col;
+S_Col 				s_col1;
+S_Col 				s_col2;
 
 // setUp is executed for each test, even if test does nothing
 void setUp(void)
 { 
+
+}
+
+void tearDown(void)
+{	
+
+}
+
+void test_s_col1and2(void)
+{
 	brain_opts 			= lu_brain_opts_create(1, 20 * 1024);
-	rec_opts_1 			= lu_rec_opts_create(brain_opts, 2, 2, 1, 0.0, 10.0, 10, .2);
+	rec_opts_1 			= lu_rec_opts_create(brain_opts, 1, 1, 1, 0.0, 10.0, 10, .2);
+	TEST_ASSERT(rec_opts_1);
+
+	rec_opts_2			= lu_rec_opts_create(brain_opts, 1, 1, 2, -3.0, 4.0, 3, .2);
+	TEST_ASSERT(rec_opts_2);
 
 	brain 				= lu_brain_create(brain_opts);
 
@@ -43,18 +65,19 @@ void setUp(void)
 	s_cell = s_rec_s_cell_get(s_rec, 0, 0);
 	TEST_ASSERT(s_cell);
 
-	s_col = s_cell_s_col_get(s_cell, 0);
-	TEST_ASSERT(s_col);
-}
+	s_col1 = s_cell_s_col_get(s_cell, 0);
+	TEST_ASSERT(s_col1);
 
-void tearDown(void)
-{	
+	s_rec = s_mem_s_rec_get(s_mem, 1);
+	TEST_ASSERT(s_rec);
+
+	s_cell = s_rec_s_cell_get(s_rec, 0, 0);
+	TEST_ASSERT(s_cell);
+
+	s_col2 = s_cell_s_col_get(s_cell, 0);
+	TEST_ASSERT(s_col2);
+
+
 	lu_brain_opts_destroy(brain_opts);
 	lu_brain_destroy(brain);
-}
-
-
-void test_s_col(void)
-{
-
 }
