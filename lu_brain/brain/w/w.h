9eeaa6b10e07e025d5b4c03ea8c3a69410d02dfc
Oleh Novosad <oleh.novosad@gmail.com>
2020-08-01 07:19:53 -0400

    In progress

--- lu_brain/brain/w/w.h
+++ lu_brain/brain/w/w.h
@@ -66,16 +66,24 @@
  	static void w_rec_data_save(W_Rec self, Data data, lu_size block_i);
 
 	struct w_mem {
-		// vlasnyk
-		Lu_Wave 				wave;
+		// Other opts
+		lu_value 				contrast_vs_color;			// blyzhche do 1 ce contrast
+		lu_value 				fire_breakpoint;
 
+		// Refs
+		Lu_Brain 				brain;
+
+		// Internal
 		Arr 					recs;
 	};
 	
 	// w_mem.lu
-	static W_Mem w_mem_create(Lu_Wave wave, S_Mem s_mem);
+	static W_Mem w_mem_init(W_Mem self, Lu_Brain brain);
 	static void w_mem_destroy(W_Mem self);
 	static W_Rec w_mem_rec_get(W_Mem self, lu_size rec_id);
+
+	// wave_save.lu
+	static void w_mem_save_async(W_Mem self, Lu_Story story);
 
 ///////////////////////////////////////////////////////////////////////////////
 // 
@@ -94,18 +102,6 @@
 	struct lu_wave {
 		struct w_mem 			super;
 
-		// Other opts
-		lu_value 				contrast_vs_color;			// blyzhche do 1 ce contrast
-		lu_value 				fire_breakpoint;
-
-		// Refs
-		Lu_Brain 				brain;
-		S_Mem 					s_mem; 
-		N_Mem 					n_mem;
-		Lu_Story 				story;
-
-		// Internal
-		W_Mem 					w_mem;
 	};
 
 	// wave.lu
@@ -117,8 +113,6 @@
 	// wave_save.lu
 	Lu_Wave lu_wave_save(Lu_Wave, Lu_Story); 
 	void lu_wave_save_async(Lu_Wave, Lu_Story);
-	static void wave_save_async_internal(Lu_Wave self, Lu_Story story);
-	static void wave_data_save(Lu_Wave self, S_Rec s_rec, Data data, lu_size block_i);
 
 	// wave_find.lu 
 	Lu_Wave lu_wave_find(Lu_Wave, Lu_Story);
