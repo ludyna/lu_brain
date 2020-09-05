65edd1dbffd577b43e7b14f08d7b0af928ea92e0
Oleh Novosad <oleh.novosad@gmail.com>
2020-09-05 04:15:31 -0400

    In progress

--- test/lib/lists/test_table_list.c
+++ test/lib/lists/test_table_list.c
@@ -26,68 +26,56 @@ void test_list1(void)
     Neu n4 = (Neu) mem_alloc(mem, sizeof(struct neu));
     Neu n5 = (Neu) mem_alloc(mem, sizeof(struct neu));
 
-    Lu_List list = lu_list_create(mem);
+    Lu_Table_List list = lu_table_list_create(mem, 10);
 
     TEST_ASSERT(list != NULL);
 
-    Lu_L_Node node1 = lu_list_append(list, n1);
+    Lu_L_Node node1 = lu_table_list_append(list, n1);
     
-    TEST_ASSERT(list->first->value == n1);
-    TEST_ASSERT(list->last->value == n1);
-    TEST_ASSERT(lu_list_count(list) == 1);
+    TEST_ASSERT(list->super.first->value == n1);
+    TEST_ASSERT(list->super.last->value == n1);
+    TEST_ASSERT(lu_table_list_count(list) == 1);
 
-    Lu_L_Node node2 = lu_list_append(list, n2);
+    Lu_L_Node node2 = lu_table_list_append(list, n2);
 
-    TEST_ASSERT(list->first->value == n1);
-    TEST_ASSERT(list->last->value == n2);
-    TEST_ASSERT(lu_list_count(list) == 2);
+    TEST_ASSERT(list->super.first->value == n1);
+    TEST_ASSERT(list->super.last->value == n2);
+    TEST_ASSERT(lu_table_list_count(list) == 2);
 
-    Lu_L_Node node3 = lu_list_prepend(list, n3);
 
-    TEST_ASSERT(lu_list_count(list) == 3);
-    TEST_ASSERT(list->first->value == n3);
-    TEST_ASSERT(list->last->value == n2);
-    TEST_ASSERT(list->first->next->value == n1);
+    Lu_L_Node node5 = lu_table_list_append(list, n5);
 
-    Lu_L_Node node5 = lu_list_append(list, n5);
+    TEST_ASSERT(lu_table_list_count(list) == 3);
+    TEST_ASSERT(list->super.first->value == n1);
+    TEST_ASSERT(list->super.last->value == n5);
+    TEST_ASSERT(list->super.last->prev->value == n2);
 
-    TEST_ASSERT(lu_list_count(list) == 4);
-    TEST_ASSERT(list->first->value == n3);
-    TEST_ASSERT(list->last->value == n5);
-    TEST_ASSERT(list->last->prev->value == n2);
+    lu_table_list_remove(list, node2);
 
-    lu_list_remove(list, node2);
+    TEST_ASSERT(lu_table_list_count(list) == 2);
+    TEST_ASSERT(list->super.first->value == n1);
+    TEST_ASSERT(list->super.last->value == n5);
+    TEST_ASSERT(list->super.last->prev->value == n1);
+ 
 
-    TEST_ASSERT(lu_list_count(list) == 3);
-    TEST_ASSERT(list->first->value == n3);
-    TEST_ASSERT(list->last->value == n5);
-    TEST_ASSERT(list->last->prev->value == n1);
+    lu_table_list_remove(list, node1);
 
-    lu_list_remove(list, node3);
+    TEST_ASSERT(lu_table_list_count(list) == 1);
+    TEST_ASSERT(list->super.first->value == n5);
+    TEST_ASSERT(list->super.last->value == n5);
+    TEST_ASSERT(list->super.last->prev == NULL);
 
-    TEST_ASSERT(lu_list_count(list) == 2);
-    TEST_ASSERT(list->first->value == n1);
-    TEST_ASSERT(list->last->value == n5);
-    TEST_ASSERT(list->last->prev->value == n1);
+    lu_table_list_remove(list, node5);
 
-    lu_list_remove(list, node1);
-
-    TEST_ASSERT(lu_list_count(list) == 1);
-    TEST_ASSERT(list->first->value == n5);
-    TEST_ASSERT(list->last->value == n5);
-    TEST_ASSERT(list->last->prev == NULL);
-
-    lu_list_remove(list, node5);
-
-    TEST_ASSERT(lu_list_count(list) == 0);
-    TEST_ASSERT(list->first == NULL);
-    TEST_ASSERT(list->last == NULL);
+    TEST_ASSERT(lu_table_list_count(list) == 0);
+    TEST_ASSERT(list->super.first == NULL);
+    TEST_ASSERT(list->super.last == NULL);
 
     mem_destroy(mem, g_mem_temp);
-    lu_list_destroy(list);
+    lu_table_list_destroy(list);
 }
 
-void test_list_fast_ma(void)
+void test_max_size(void)
 {
     Mem mem     = (Mem) mem_perm_create(g_mem_temp, 2048);
 
@@ -97,25 +85,21 @@ void test_list_fast_ma(void)
     Neu n4 = (Neu) mem_alloc(mem, sizeof(struct neu));
     Neu n5 = (Neu) mem_alloc(mem, sizeof(struct neu));
 
-    Lu_List list = lu_list_create(mem);
+    Lu_Table_List list = lu_table_list_create(mem, 3);
 
-    Lu_L_Node node1 = lu_list_append(list, n1);
-    Lu_L_Node node2 = lu_list_append(list, n2);
-    Lu_L_Node node3 = lu_list_append(list, n3);
+    Lu_L_Node node1 = lu_table_list_append(list, n1);
+    Lu_L_Node node2 = lu_table_list_append(list, n2);
+    Lu_L_Node node3 = lu_table_list_append(list, n3);
 
-    TEST_ASSERT(lu_list_count(list) == 3);
-    TEST_ASSERT(list->first->value == n1);
-    TEST_ASSERT(list->last->value == n3);
-    TEST_ASSERT(list->first->next->value == n2);
+    TEST_ASSERT(lu_table_list_count(list) == 3);
+    TEST_ASSERT(list->super.first->value == n1);
+    TEST_ASSERT(list->super.last->value == n3);
+    TEST_ASSERT(list->super.first->next->value == n2);
 
-    Lu_L_Node node4 = lu_list_prepend(list, n4);
-    TEST_ASSERT(lu_list_count(list) == 4);
-    TEST_ASSERT(list->first->value == n4);
-
-    lu_list_remove(list, node2);
-
-    TEST_ASSERT(list->count == 3);
+    Lu_L_Node node4 = lu_table_list_append(list, n4);
+    TEST_ASSERT(lu_table_list_count(list) == 3);
+    TEST_ASSERT(list->super.first->value == n2);
 
     mem_destroy(mem, g_mem_temp);
-    lu_list_destroy(list);
+    lu_table_list_destroy(list);
 }
