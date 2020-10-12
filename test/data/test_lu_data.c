7fb59306c43367cf7136f73b08dbf933f0ae15c7
Oleh Novosad <oleh.novosad@gmail.com>
2020-10-12 18:32:46 -0400

    In progress

--- test/data/test_lu_data.c
+++ test/data/test_lu_data.c
@@ -32,7 +32,37 @@ void test_lu_data_create_via_shift(void)
 {
 	Lu_Data data_1 = lu_data_create_via_shift(g_mem_temp, &data_0, -1, 0);
 
-	lu_data_debug(data_1);
+	// lu_data_debug(data_1);
+
+	TEST_ASSERT(lu_data_value_get(data_1, 0, 0, 0) == 1);
+	TEST_ASSERT(lu_data_value_get(data_1, 1, 1, 0) == 4);
+
+	lu_data_destroy(data_1, g_mem_temp);
+
+	data_1 = lu_data_create_via_shift(g_mem_temp, &data_0, 1, 0);
+
+	// lu_data_debug(data_1);
+
+	TEST_ASSERT(lu_data_value_get(data_1, 0, 0, 0) == 2);
+	TEST_ASSERT(lu_data_value_get(data_1, 1, 1, 0) == 6);
+
+	lu_data_destroy(data_1, g_mem_temp);
+
+	data_1 = lu_data_create_via_shift(g_mem_temp, &data_0, 0, -1);
+
+	// lu_data_debug(data_1);
+
+	TEST_ASSERT(lu_data_value_get(data_1, 0, 0, 0) == 1);
+	TEST_ASSERT(lu_data_value_get(data_1, 1, 1, 0) == 2);
+
+	lu_data_destroy(data_1, g_mem_temp);
+
+	data_1 = lu_data_create_via_shift(g_mem_temp, &data_0, 1, 1);
+
+	// lu_data_debug(data_1);
+
+	TEST_ASSERT(lu_data_value_get(data_1, 0, 0, 0) == 5);
+	TEST_ASSERT(lu_data_value_get(data_1, 1, 1, 0) == 9);
 
 	lu_data_destroy(data_1, g_mem_temp);
 }
