2b4dd65efd7f9bc4bb81b2cf4349e410644b5848
Oleh Novosad <oleh.novosad@gmail.com>
2021-04-07 08:34:59 -0400

    HNN in progress

--- lu_brain/lib/n/hnn/_module.h
+++ lu_brain/lib/n/hnn/_module.h
@@ -4,5 +4,5 @@
 
 #include "_typedefs.h"
 #include "hnn_cell.h"
-#include "hnn_cell_allocator.h"
 #include "hnn_table.h"
+#include "hnn_cell_allocator.h"
--- lu_brain/lib/n/hnn/hnn_table.h
+++ lu_brain/lib/n/hnn/hnn_table.h
@@ -2,16 +2,24 @@
 	Copyright © 2021 Oleh Ihorovych Novosad 
 */
 
-struct hnn_table {
+///////////////////////////////////////////////////////////////////////////////
+// 
+	struct hnn_table {
 
-};
+		Hnn_Cell* table;
 
-static Hnn_Table hnn_table_create(lu_size size_in_cells);
-static void hnn_table_destroy(Hnn_Table self);
+	};
+ 
+///////////////////////////////////////////////////////////////////////////////
+// 
+	static Hnn_Table hnn_table_create(lu_size size_in_cells);
+	static void hnn_table_destroy(Hnn_Table self);
 
+///////////////////////////////////////////////////////////////////////////////
+//  
 
-static Hnn_Cell hnn_table_get_2(Hnn_Table self, lu_size shift_1, lu_size shift_2);
-static Hnn_Cell hnn_table_get_4(Hnn_Table self, lu_size sh_1, lu_size sh_2, lu_size sh_3, lu_size sh_4);
+	static Hnn_Cell hnn_table_get_or_create_1(Hnn_Table self, lu_size sh_1, lu_byte type);
+	static Hnn_Cell hnn_table_get_or_create_2(Hnn_Table self, lu_size shift_1, lu_size shift_2, lu_byte type);
+	static Hnn_Cell hnn_table_get_or_create_3(Hnn_Table self, lu_size sh_1, lu_size sh_2, lu_size sh_3, lu_byte type);
+	static Hnn_Cell hnn_table_get_or_create_4(Hnn_Table self, lu_size sh_1, lu_size sh_2, lu_size sh_3, lu_size sh_4, lu_byte type);
 
-static Hnn_Cell hnn_table_create_2(Hnn_Table self, lu_size sh_1, lu_size sh_2);
-static Hnn_Cell_4 hnn_table_create_4(Hnn_Table self, lu_size sh_1, lu_size sh_2, lu_size sh_3, lu_size sh_4);
