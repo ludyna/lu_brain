c956ee422115942c171a53a2155f6e9bf02d1abe
Oleh Novosad <oleh.novosad@gmail.com>
2021-04-08 05:22:35 -0400

    HNN In progress

--- lu_brain/lib/n/hnn/_typedefs.h
+++ lu_brain/lib/n/hnn/_typedefs.h
@@ -6,6 +6,10 @@ typedef struct hnn_cell* Hnn_Cell;
 typedef struct hnn_cell_2* Hnn_Cell_2; 
 typedef struct hnn_cell_3* Hnn_Cell_3;
 typedef struct hnn_cell_4* Hnn_Cell_4;
-typedef struct hnn_table* Hnn_Table;
 typedef struct hnn_cell_allocator* Hnn_Cell_Allocator;
+
+typedef struct hnn_table* Hnn_Table;
+typedef struct hnn_links* Hnn_Links;
+
+typedef struct hnn_config* Hnn_Config;
 typedef struct hnn* Hnn;
--- lu_brain/lib/n/hnn/hnn_links.h
+++ lu_brain/lib/n/hnn/hnn_links.h
@@ -4,3 +4,14 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 // 
+
+	struct hnn_links {
+
+		Hnn_Table table_1;
+		Hnn_Table table_2;
+		Hnn_Table table_3;
+		Hnn_Table table_4;
+
+	};
+
+	Hnn_Links hnn_links_create();
