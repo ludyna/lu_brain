a3b9ec4ff5db31073817f23de74daf64ea502704
Oleh Novosad <oleh.novosad@gmail.com>
2021-04-08 02:23:58 -0400

    HNN in progress

--- lu_brain/lib/n/hnn/hnn.h
+++ lu_brain/lib/n/hnn/hnn.h
@@ -5,8 +5,14 @@
 ///////////////////////////////////////////////////////////////////////////////
 // 
 
+	struct hnn_config {
+
+	};
+
 	struct hnn {
 
+		Hnn_Cell_Allocator cell_allocator;
+		
 	};
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -14,3 +20,9 @@
 
 	Hnn_Cell hnn_cell_spawn(Hnn self, lu_byte type);
 	void hnn_cell_die(Hnn self, Hnn_Cell cell);
+
+	Hnn_Cell hnn_connect_2(Hnn self, Hnn_Cell c1, Hnn_Cell c2);
+	Hnn_Cell hnn_connect_3(Hnn self, Hnn_Cell c1, Hnn_Cell c2, Hnn_Cell c3);
+	Hnn_Cell hnn_connect_4(Hnn self, Hnn_Cell c1, Hnn_Cell c2, Hnn_Cell c3, Hnn_Cell c4);
+
+
