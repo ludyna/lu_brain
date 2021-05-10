5a1e7055a05c9b63cde5a8fef927b84954b7b2e1
Oleh Novosad <oleh.novosad@gmail.com>
2021-05-10 13:53:05 -0400

    n_mem in progress

--- lu_brain/lib/n/n_mem.h
+++ lu_brain/lib/n/n_mem.h
@@ -4,6 +4,12 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 // N_Mem
+
+	struct n_mem_config {
+		lu_size names_size;
+
+		struct hnn_config hnn_config;
+	};
 	
 	struct n_mem {
 		// vlasnyk
@@ -13,20 +19,14 @@
 		Simple_Hash				names;
 	};
 
-	// n_mem_creates.lu
+
+///////////////////////////////////////////////////////////////////////////////
+// N_Mem Create & Destroy
+
 	static N_Mem n_mem_create(Lu_Brain brain, lu_size names_size);
 	static void n_mem_destroy(N_Mem self);
-	static N_Mem n_mem_tables_create(N_Mem self, Mem mem);
-	static Mem_Table n_mem_cells_2_create(N_Mem self, Mem mem);
-	static Mem_Table n_mem_cells_create(N_Mem self, Mem mem);
-	static Mem_Table n_mem_cells_3_create(N_Mem self, Mem mem);
-	static Mem_Table n_mem_lins_create(N_Mem self, Mem mem);
 
-	// n_mem.lu
+///////////////////////////////////////////////////////////////////////////////
+// N_Mem Utility
+	
 	static void n_mem_print_info(N_Mem);
-
-	// n_mem_allocs.lu
-	static N_Cell n_mem_cell_alloc(N_Mem self);
-	static N_Cell_2 n_mem_cell_2_alloc(N_Mem self);
-	static N_Cell_3 n_mem_cell_3_alloc(N_Mem self);
-	static N_Lin n_mem_lin_alloc(N_Mem self);
