2a9f0ec7d1b902f75425ceabd2d598c576f0cc10
Oleh Novosad <oleh.novosad@gmail.com>
2021-06-10 15:54:59 -0400

    waves in progress'

--- lu_brain/lib/story/story.h
+++ lu_brain/lib/story/story.h
@@ -6,13 +6,16 @@
 // Lu_Block 
 
 	struct lu_block {
-		Lu_Story 			story;
+		Lu_Mem mem;
 		lu_size 			datum_size;
+
+		// Using "struct lu_data*" here istead of Lu_Data to point out that 
+		// datum is array of "struct lu_data"
 		struct lu_data* 	datum;
 
 	};
 
-	static Lu_Block lu_block_create(Lu_Story story);
+	static Lu_Block lu_block_create(Lu_Mem mem, lu_size recs_size);
 	static void lu_block_destroy(Lu_Block self);
 
 	static inline Lu_Data lu_block_data_get(Lu_Block self, lu_size index) { return &self->datum[index]; }
@@ -21,17 +24,15 @@
 // Lu_Story 
 
 	struct lu_story {
-		// Not resetable
-		Lu_Brain 			brain;
 		Lu_Mem 				mem;
+		lu_size 			recs_size;
 
 		Lu_List 			blocks;
 
-		// Resetable
 		lu_bool 			start_block_on_next_data;
 	};
 
-	Lu_Story lu_story_create(Lu_Brain brain, Lu_Mem mem);
+	Lu_Story lu_story_create(Lu_Mem mem, lu_size recs_size);
 	void lu_story_destroy(Lu_Story self);
 
 	void lu_story_block_start(Lu_Story story);
@@ -42,4 +43,4 @@
 	Lu_Story lu_story_prepare(Lu_Story);
 	static Lu_Block lu_story_block_add(Lu_Story self);
 	static inline lu_bool lu_story_is_empty(Lu_Story self) { return lu_list_is_empty(self->blocks); }
-	static inline lu_size lu_story_recs_count(Lu_Story self) { return lu_brain_recs_size(self->brain); }
+	static inline lu_size lu_story_recs_count(Lu_Story self) { return self->recs_size; }
