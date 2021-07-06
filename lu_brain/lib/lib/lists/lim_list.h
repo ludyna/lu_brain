0c03e2cf9284500a98e3a3b7c4144519573ad390
Oleh Novosad <oleh.novosad@gmail.com>
2021-07-06 13:06:31 -0400

    wave in progress

--- lu_brain/lib/lib/lists/lim_list.h
+++ lu_brain/lib/lib/lists/lim_list.h
@@ -9,7 +9,7 @@
 	struct lu_lim_list {
 		struct lu_list 	super;
 
-		Lu_Mem_Table 		mem_table;
+		Lu_Mem_Table 	mem_table;
 		lu_size 		max_size;
 
 		// We need callbacks feature for this data structure because this data structure 
@@ -19,10 +19,17 @@
 		void 			(*node_before_destroy)(Lu_Lim_List, Lu_L_Node);
 	};
 
+	// Properties
+
 	static inline lu_size lu_lim_list_count(Lu_Lim_List self) { return lu_list_count(&self->super); }
 	static inline lu_size lu_lim_list_size(Lu_Lim_List self) { return self->max_size; }
 	static inline lu_bool lu_lim_list_is_empty(Lu_Lim_List self) { return lu_list_count(&self->super) == 0; }
 
+	static inline Lu_L_Node lu_lim_list_node_last(Lu_Lim_List self) { return self->super.last; }
+	static inline Lu_L_Node lu_lim_list_node_first(Lu_Lim_List self) { return self->super.first; }
+	
+	// Init
+	
 	static inline Lu_Lim_List lu_lim_list_init(Lu_Lim_List self, Lu_Mem_Table mem_table, lu_size limit_size)
 	{
 		lu_assert(self);
@@ -37,11 +44,12 @@
 		return self;
 	}
 	
+	// Create and destroy
+
 	Lu_Lim_List lu_lim_list_create(Lu_Mem, lu_size);
 	void lu_lim_list_destroy(Lu_Lim_List self);
 
-	static inline Lu_L_Node lu_lim_list_node_last(Lu_Lim_List self) { return self->super.last; }
-	static inline Lu_L_Node lu_lim_list_node_first(Lu_Lim_List self) { return self->super.first; }
-	
+	// Methods
+
 	Lu_L_Node lu_lim_list_append(Lu_Lim_List, lu_p_void);
 	void lu_lim_list_node_remove(Lu_Lim_List, Lu_L_Node);
