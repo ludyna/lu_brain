8993fac40eb5f32af29095dde76a08b53c730ec2
Oleh Novosad <oleh.novosad@gmail.com>
2021-10-18 18:22:06 -0400

    s in prog

--- lu_brain/lib/s/_typedefs.h
+++ lu_brain/lib/s/_typedefs.h
@@ -6,45 +6,21 @@
 ///////////////////////////////////////////////////////////////////////////////
 // S
 
-	// 
-	// Slots
-	// 
-
-	typedef struct lu_s_slot_1* Lu_S_Slot_1;
-	typedef struct lu_s_slot_2* Lu_S_Slot_2;
-	typedef struct lu_s_slot_4* Lu_S_Slot_4;
-
-	//
-	// Cells
-	//
-
-	enum lu_s_cell_type {
-		LU_SCT_COMPONTENT,
-		LU_SCT_PIXEL,
-		LU_SCT_REC,
-		LU_SCT_SEQ_NX,
-		LU_SCT_SEQ,
-		LU_SCT_STORY_NX
-	};
-
- 	typedef struct lu_s_cell_base* 				Lu_S_Cell_Base;
- 	typedef struct lu_s_comp_cell* 				Lu_S_Comp_Cell;
-
-	// 
-	// Cell_Mem
-	//
-
-	typedef struct lu_s_cell_mem_config* Lu_S_Cell_Mem_Config;
-	typedef struct lu_s_cell_mem*   	Lu_S_Cell_Mem;
-
-
 	//
 	// Layer
 	//
 
 	typedef struct lu_s_layer_base* Lu_S_Layer_Base;
 	typedef struct lu_s_layer* Lu_S_Layer;
+	typedef struct lu_s_comp_layer* Lu_S_Comp_Layer;
 
+
+	// 
+	// Layer_Mem
+	//
+
+	typedef struct lu_s_layer_mem_config* Lu_S_Layer_Mem_Config;
+	typedef struct lu_s_layer_mem* Lu_S_Layer_Mem;
 
 	//
 	// S
--- lu_brain/lib/s/s.h
+++ lu_brain/lib/s/s.h
@@ -37,7 +37,6 @@
 	pixel(w Y h)
 	comp1 comp2 comp3
 
-
 	r1 r2 r3 r4
 	r1 r2 r3 r4
 	r1 r2 r3 r4
@@ -50,159 +49,11 @@
 	Vin maye maty mozhlyvist dobavliaty yih stilky skilky zahoche i nazyvaty yih yak zahoche 
 	(slova, rechenia, abzacy, storinky i td).
 	Yaksho programist kazhe startuyem novyy level3, ce ozanachaye avtomatychno novyy level 2 and level 1.
+
+	There will not be horizontal growing fro S, only vertical, except for comps layers.
+	No need for S cells. No need for S slots.
 */
 
-///////////////////////////////////////////////////////////////////////////////
-// Slots
-//
-
-	struct lu_s_slot_1 {
-		Lu_S_Cell_Base one;
-	};
-
-	struct lu_s_slot_4 {
-		Lu_S_Cell_Base one;
-		Lu_S_Cell_Base two;
-		Lu_S_Cell_Base three;
-		Lu_S_Cell_Base four;
-	};
-
-	struct lu_s_slot_2 {
-		Lu_S_Cell_Base one;
-		Lu_S_Cell_Base two;
-	};
-
-	//
-	// Slot inits
-	//
-
-	static inline Lu_S_Slot_1 lu_s_slot_1__init(Lu_S_Slot_1 self) 
-	{
-		self->one = NULL;
-		return self;
-	}
-
-	static inline Lu_S_Slot_2 lu_s_slot_2__init(Lu_S_Slot_2 self) 
-	{
-		self->one = NULL;
-		self->two = NULL;
-		return self;
-	} 
-
-	static inline Lu_S_Slot_4 lu_s_slot_4__init(Lu_S_Slot_4 self) 
-	{
-		self->one = NULL;
-		self->two = NULL;
-		self->three = NULL;
-		self->four = NULL;
-		return self;
-	}
-
-///////////////////////////////////////////////////////////////////////////////
-// Cells
-// 
-
-	struct lu_s_cell_base {
-		enum lu_s_cell_type type;			
-
-		// for S and W it is always one parent
-		struct lu_s_slot_1 p; 
-	}; 
-
-	struct lu_s_cell_2 {
-		struct lu_s_cell_base super;
-
-		struct lu_s_slot_2 c;
-	};
-
-	// struct lu_s_cell_4 {
-	// 	struct lu_s_cell_base super;
-
-	// 	struct lu_s_slot_2
-	// }
-
-	// can be v or p
-	struct lu_s_comp_cell {
-		struct lu_s_cell_base super;
-
-		lu_value 				orig_min;
-		lu_value 				orig_max;
-		lu_value 				max;
-
-		lu_value 				step;
-		lu_value*				steps;  		// preobchysleni kroky
-		lu_size 				cells_size;
-
-		// w x h x cells_size_i, empty by default
-		Hnn_Cell_Value* cells;
-	};
-
-	static Lu_S_Comp_Cell lu_s_comp_cell__init(Lu_S_Comp_Cell, Lu_Mem mem, lu_value min, lu_value max, lu_size cells_size);
-	static void lu_s_comp_cell__deinit(Lu_S_Comp_Cell self);
-
-	static inline lu_value lu_s_comp_cell__norm(Lu_S_Comp_Cell self, lu_value request);
-	static inline lu_size lu_s_comp_cell__ix(Lu_S_Comp_Cell self, lu_value val);
-	static inline struct lu_size_range lu_s_comp_cell__ix_range(Lu_S_Comp_Cell self, lu_value val, lu_size nsc);
-	static inline lu_value lu_s_comp_cell__calc_sig(Lu_S_Comp_Cell self, lu_size val_step_i, lu_value val);
-	static inline lu_value lu_s_comp_cell__step_norm_dist(Lu_S_Comp_Cell self);
-
-
-///////////////////////////////////////////////////////////////////////////////
-// Lu_S_Cell_Mem
-//
-	struct lu_s_cell_mem_config {
-		lu_size component_cells_size;
-
-	};
-
-	static inline Lu_S_Cell_Mem_Config lu_s_cell_mem_config__init(Lu_S_Cell_Mem_Config self)
-	{
-		lu__assert(self);
-
-		self->component_cells_size = 0;
-
-
-		return self;
-	}
-
-	static inline Lu_S_Cell_Mem_Config lu_s_cell_mem_config__validate(Lu_S_Cell_Mem_Config self)
-	{
-		lu__assert(self);
-
-		lu__assert(self->component_cells_size > 0);
-
-		return self;
-	}
-
-	struct lu_s_cell_mem {
-		Lu_Mem mem;
-
-		struct lu_s_cell_mem_config config;
-
-		lu_size component_cells_count;
-		struct lu_s_comp_cell* component_cells;
-
-
-	};
-
-	//
-	// Create and Destroy
-	// 
-
-	static Lu_S_Cell_Mem lu_s_cell_mem__create(Lu_Mem mem, struct lu_s_cell_mem_config config);
-	static void lu_s_cell_mem__destroy(Lu_S_Cell_Mem self);
-
-	//
-	// Allocate cells
-	// 
-
-	static void lu_s_cell_mem__alloc_cells(Lu_S_Cell_Mem self, struct lu_s_cell_mem_config config);
-
-	// 
-	// Print
-	//
-
-	static void lu_s_cell_mem__print_info(Lu_S_Cell_Mem self);
 
 ///////////////////////////////////////////////////////////////////////////////
 // Lu_S_Layer
@@ -216,6 +67,91 @@
 		struct lu_s_layer_base super;
 	};
 
+	// can be v or p
+	struct lu_s_comp_layer {
+
+		struct lu_s_layer_base 	super;
+
+		lu_value 				orig_min;
+		lu_value 				orig_max;
+		lu_value 				max;
+
+		lu_value 				step;
+		lu_value*				steps;  		// preobchysleni kroky
+		lu_size 				cells_size;
+
+		// w x h x cells_size_i, empty by default
+		Hnn_Cell_Value* cells;
+	};
+
+	static Lu_S_Comp_Layer lu_s_comp_cell__init(Lu_S_Comp_Layer, Lu_Mem mem, lu_value min, lu_value max, lu_size cells_size);
+	static void lu_s_comp_cell__deinit(Lu_S_Comp_Layer self);
+
+	static inline lu_value lu_s_comp_cell__norm(Lu_S_Comp_Layer self, lu_value request);
+	static inline lu_size lu_s_comp_cell__ix(Lu_S_Comp_Layer self, lu_value val);
+	static inline struct lu_size_range lu_s_comp_cell__ix_range(Lu_S_Comp_Layer self, lu_value val, lu_size nsc);
+	static inline lu_value lu_s_comp_cell__calc_sig(Lu_S_Comp_Layer self, lu_size val_step_i, lu_value val);
+	static inline lu_value lu_s_comp_cell__step_norm_dist(Lu_S_Comp_Layer self);
+
+
+///////////////////////////////////////////////////////////////////////////////
+// Lu_S_Layer_Mem
+//
+	struct lu_s_layer_mem_config {
+		lu_size component_cells_size;
+
+	};
+
+	static inline Lu_S_Layer_Mem_Config lu_s_cell_mem_config__init(Lu_S_Layer_Mem_Config self)
+	{
+		lu__assert(self);
+
+		self->component_cells_size = 0;
+
+
+		return self;
+	}
+
+	static inline Lu_S_Layer_Mem_Config lu_s_cell_mem_config__validate(Lu_S_Layer_Mem_Config self)
+	{
+		lu__assert(self);
+
+		lu__assert(self->component_cells_size > 0);
+
+		return self;
+	}
+
+	struct lu_s_layer_mem {
+		Lu_Mem mem;
+
+		struct lu_s_layer_mem_config config;
+
+		lu_size component_cells_count;
+		struct lu_s_comp_layer* component_cells;
+
+
+	};
+
+	//
+	// Create and Destroy
+	// 
+
+	static Lu_S_Layer_Mem lu_s_cell_mem__create(Lu_Mem mem, struct lu_s_layer_mem_config config);
+	static void lu_s_cell_mem__destroy(Lu_S_Layer_Mem self);
+
+	//
+	// Allocate cells
+	// 
+
+	static void lu_s_cell_mem__alloc_cells(Lu_S_Layer_Mem self, struct lu_s_layer_mem_config config);
+
+	// 
+	// Print
+	//
+
+	static void lu_s_cell_mem__print_info(Lu_S_Layer_Mem self);
+
+
 ///////////////////////////////////////////////////////////////////////////////
 // Lu_S
 //
@@ -223,7 +159,7 @@
 	struct lu_s {
 
 		Lu_Mem 					mem;
-		Lu_S_Cell_Mem  			cell_mem;
+		Lu_S_Layer_Mem  			cell_mem;
 
 
 		Lu_S_Layer 				top;
