5c9f146387effcc4c03535f5bd5e498ed29b86a5
Oleh Novosad <oleh.novosad@gmail.com>
2021-10-18 20:17:28 -0400

    s in prog

--- lu_brain/lib/s/_typedefs.h
+++ lu_brain/lib/s/_typedefs.h
@@ -10,6 +10,15 @@
 	// Layer
 	//
 
+	enum lu_s_layer_type {
+		LU_S_LT_COMP_V,
+		LU_S_LT_COMP_P,
+		LU_S_LT_PIXEL,
+		LU_S_LT_4,
+		LU_S_LT_2,
+		LU_S_LT_END
+	};
+
 	typedef struct lu_s_layer_base* Lu_S_Layer_Base;
 	typedef struct lu_s_layer* Lu_S_Layer;
 	typedef struct lu_s_comp_layer* Lu_S_Comp_Layer;
--- lu_brain/lib/s/s.h
+++ lu_brain/lib/s/s.h
@@ -1,41 +1,19 @@
 /**
 	Copyright © 2021 Oleh Ihorovych Novosad 
 
-	Map:
-		comp (d) 
-			comp_cell component_v x n(cells_size_i, empty by default) ==>
-			comp_cell component_p x n(cells_size_i, empty by default) ==> 
-
-			pixel_nx (w Y h)
-				pixel (w - 1 Y h - 1)
-					...
-						rec_nx (recs_count Y t)
-							rec(recs_count - 1 Y t - 1)
-								...
-									seq_nx (seq_count)
-										seq(seq_count - 1)
-											...
-												event_nx (event_count)
-													event_nx (event_count - 1)
-														...
-															scene_nx (chapter_count)
-																..
-																	story_nx (story_count)
-
-														
-
-	(x Y y) = max(x - 1, 1) * max(y - 1, 1)
+	Map:													
+		(x Y y) = max(x - 1, 1) * max(y - 1, 1)
 
 
-	"Minimal" start:
+		"Minimal" start:
 
-	level3, story(1)
-	level2, scene(1)
-	level1, event (1)
-	seq (1)
-	rec (rec_count)
-	pixel(w Y h)
-	comp1 comp2 comp3
+		level3, story(vertically 1-n4)
+		level2, scene(vertically 1-n3)
+		level1, event (vertically 1-n2)
+		seq (vertically 1-n1)
+		rec (vertically rec_count)
+		pixel(vertically w Y h)
+		comp1 comp2 comp3 (horizontally comp_count or d)
 
 	r1 r2 r3 r4
 	r1 r2 r3 r4
@@ -52,6 +30,10 @@
 
 	There will not be horizontal growing fro S, only vertical, except for comps layers.
 	No need for S cells. No need for S slots.
+
+	S and W wil be very similar.
+
+	We build not from top and not from bottom, we build from pixel layer.
 */
 
 
@@ -59,13 +41,29 @@
 // Lu_S_Layer
 //
 
-	struct lu_s_layer_base {
+	//
+	// Base
+	//
 
+	struct lu_s_layer_base {
+		enum lu_s_layer_type type;
+		lu_size level;
 	};
- 
-	struct lu_s_layer {
-		struct lu_s_layer_base super;
-	};
+
+	static inline Lu_S_Layer_Base lu_s_layer_base__init(Lu_S_Layer_Base self, enum lu_s_layer_type type, lu_size level)
+	{
+		lu__assert(self);
+		lu__assert(type < LU_S_LT_END);
+
+		self->type = type;
+		self->level = level;
+
+		return self;
+	}
+	
+	//
+	// Comp_Layer
+	//
 
 	// can be v or p
 	struct lu_s_comp_layer {
@@ -84,14 +82,41 @@
 		Hnn_Cell_Value* cells;
 	};
 
-	static Lu_S_Comp_Layer lu_s_comp_cell__init(Lu_S_Comp_Layer, Lu_Mem mem, lu_value min, lu_value max, lu_size cells_size);
-	static void lu_s_comp_cell__deinit(Lu_S_Comp_Layer self);
+	static Lu_S_Comp_Layer lu_s_comp_layer__init(Lu_S_Comp_Layer, Lu_Mem mem, lu_value min, lu_value max, lu_size cells_size);
+	static void lu_s_comp_layer__deinit(Lu_S_Comp_Layer self);
 
-	static inline lu_value lu_s_comp_cell__norm(Lu_S_Comp_Layer self, lu_value request);
-	static inline lu_size lu_s_comp_cell__ix(Lu_S_Comp_Layer self, lu_value val);
-	static inline struct lu_size_range lu_s_comp_cell__ix_range(Lu_S_Comp_Layer self, lu_value val, lu_size nsc);
-	static inline lu_value lu_s_comp_cell__calc_sig(Lu_S_Comp_Layer self, lu_size val_step_i, lu_value val);
-	static inline lu_value lu_s_comp_cell__step_norm_dist(Lu_S_Comp_Layer self);
+	static inline lu_value lu_s_comp_layer__norm(Lu_S_Comp_Layer self, lu_value request);
+	static inline lu_size lu_s_comp_layer__ix(Lu_S_Comp_Layer self, lu_value val);
+	static inline struct lu_size_range lu_s_comp_layer__ix_range(Lu_S_Comp_Layer self, lu_value val, lu_size nsc);
+	static inline lu_value lu_s_comp_layer__calc_sig(Lu_S_Comp_Layer self, lu_size val_step_i, lu_value val);
+	static inline lu_value lu_s_comp_layer__step_norm_dist(Lu_S_Comp_Layer self);
+
+
+
+	//
+	// Pixel_Layer
+	//
+
+	struct lu_s_pixel_layer {
+		struct lu_s_layer_base super;
+	};
+
+	//
+	// Layer_4
+	//
+
+	struct lu_s_layer_4 {
+		struct lu_s_layer_base super;
+	};
+
+	//
+	// Layer_2
+	//
+
+	struct lu_s_layer_2 {
+		struct lu_s_layer_base super;
+	};
+
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -156,28 +181,29 @@
 // Lu_S
 //
 
+
 	struct lu_s {
 
 		Lu_Mem 					mem;
-		Lu_S_Layer_Mem  			cell_mem;
+		Lu_S_Layer_Mem  		cell_mem;
 
 
-		Lu_S_Layer 				top;
-		Lu_S_Layer 				seq;
+		Lu_S_Layer_Base 		apex;
+		Lu_S_Layer_Base 		seq;
 
 	};
 	
 	static Lu_S lu_s__create(Lu_Mem mem, Lu_Arr lu_recs);
 	static void lu_s__destroy(Lu_S self);
 
+	static void lu_s__add_rec(Lu_S self, Lu_Rec rec);
 	static void lu_s__add_layer(Lu_S self, lu_size level);
-	
 	#define lu_s__add_seq(self) lu_s__add_layer(self, 0)
 	#define lu_s__add_event(self) lu_s__add_layer(self, 1)
 	#define lu_s__add_chapter(self) lu_s__add_layer(self, 2)
 	#define lu_s__add_story(self) lu_s__add_layer(self, 3)
 
-	static void lu_s__add_rec(Lu_S self, Lu_Rec rec);
+
 
 	static void lu_s__print_info(Lu_S self);
 
