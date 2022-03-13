756a5df68bd38d4746dd97f3abdc4ed8a8e24fc8
Oleh Novosad <oleh.novosad@gmail.com>
2022-03-13 19:21:53 -0400

    in prog

--- lu_brain/lib/w/w.h
+++ lu_brain/lib/w/w.h
@@ -19,6 +19,7 @@
 		lu_byte state;
 		lu_value p1;
 		lu_value p2;
+		lu_value p;
 	};
 
 	#define LU_W_CELL_P__STATE_START 0
@@ -35,9 +36,11 @@
 		is_reset && (self->state = LU_W_CELL_P__STATE_START);
 		self->state == LU_W_CELL_P__STATE_START && (self->p1 = p);
 		self->state == LU_W_CELL_P__STATE_ONE && (self->p2 = p); 
+
 		++self->state; 
 
 		lu__assert(self->state <= LU_W_CELL_P__STATE_READY);
+		// self->state > LU_W_CELL_P__STATE_READY && self->state == LU_W_CELL_P__STATE_START;
 	}
 
 	static inline lu_bool lu_w_cell_p__is_ready(Lu_W_Cell_P self)
@@ -47,7 +50,9 @@
 
 	static inline void lu_w_cell_p__save(Lu_W_Cell_P self, Lu_Comp_Calc comp_calc, Lu_Process_Config config)
 	{
-
+		self->p = self->p2 - sefl->p1;
+		self->p > 0 && (self->p = -sefl->p); 
+		self->p = lu_comp_calc__digitalize_value(comp_calc, self->p);
 	}
 
 	struct lu_w_cell_v {
