4c0ab1488f728e52ab0d324e49ae63a0c4ed9cc7
Oleh Novosad <oleh.novosad@gmail.com>
2022-03-20 19:59:54 -0400

    in prog

--- lu_brain/lib/n/_typedefs.h
+++ lu_brain/lib/n/_typedefs.h
@@ -17,21 +17,22 @@ enum n_cell_type {
 
 typedef struct lu_n_cell_base* Lu_N_Cell_Base; 
 typedef struct lu_n_cell_vp* Lu_N_Cell_VP;
-typedef struct lu_n_cell_1* Lu_N_Cell_1;
-typedef struct lu_n_cell_2* Lu_N_Cell_2; 
-typedef struct lu_n_cell_3* Lu_N_Cell_3;
-typedef struct lu_n_cell_4* Lu_N_Cell_4;
-typedef struct lu_n_cell_allocator* Lu_N_Cell_Allocator;
+// typedef struct lu_n_cell_1* Lu_N_Cell_1;
+// typedef struct lu_n_cell_2* Lu_N_Cell_2; 
+// typedef struct lu_n_cell_3* Lu_N_Cell_3;
+// typedef struct lu_n_cell_4* Lu_N_Cell_4;
+// typedef struct lu_n_cell_allocator* Lu_N_Cell_Allocator;
 
-typedef struct lu_n_column_node* Lu_N_Column_Node;
-typedef struct lu_n_column* Lu_N_Column;
-typedef struct lu_n_links* Lu_N_Links;
+// typedef struct lu_n_column_node* Lu_N_Column_Node;
 
-typedef struct lu_n_table_comp* Lu_N_Table_Comp;
+
+
 
 typedef struct lu_n_cell* Lu_N_Cell;
+typedef struct lu_n_string* Lu_N_String;
 typedef struct lu_n_links* Lu_N_Links;
 typedef struct lu_n_free_links* Lu_N_Free_Links;
-typedef struct lu_n_string* Lu_N_String;
+typedef struct lu_n_column* Lu_N_Column;
+typedef struct lu_n_table_comp* Lu_N_Table_Comp;
 typedef struct lu_n_table* Lu_N_Table;
 
