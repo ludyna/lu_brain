26e149a417b94255063cd5e7eb14fee2efd6f555
Oleh Novosad <oleh.novosad@gmail.com>
2022-04-18 11:01:36 -0400

    in prog

--- lu_brain/lib/l/l.h
+++ lu_brain/lib/l/l.h
@@ -16,6 +16,24 @@
 		lu_size value;
 	};
 
+	struct lu_l_cell {
+		union lu_n_addr add;
+		union lu_l_label* labels;
+	};
+
+	struct lu_l_label_cell {
+
+	};
+
 	struct lu_l_table {
-		//union lu_l_label
+		Lu_Mem mem;
+		
+		lu_size h;
+		lu_size d;
+
+		// h x d hash/map
+		struct lu_l_cell *addr_to_labels;
+
+		// Label to addrs, where lu_list contains address (&) to Lu_L_Cell in addr_to_labels.
+		struct lu_list *label_to_addrs;
 	};
