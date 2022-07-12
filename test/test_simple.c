b0b8bad3c3805e951974f32ad7d45a79b4cc3bb7
Oleh Novosad <oleh.novosad@gmail.com>
2022-07-12 11:46:54 -0400

    in prog

--- test/test_simple.c
+++ test/test_simple.c
@@ -28,11 +28,23 @@ lu_value values[][4] = {
 	{ 
 		0, 1, 
 		0, 0
+	},
+	{ 
+		1, 0, 
+		1, 1
+	},
+	{ 
+		1, 0, 
+		0, 1
+	},
+	{ 
+		0, 0, 
+		1, 0
 	}
 };
 
 
-lu_size labels[] = { 0, 1 };
+lu_size labels[] = { 0, 1, 2, 3, 4 };
 
 // setUp is executed for each test, even if test does nothing
 void setUp(void)
@@ -123,7 +135,7 @@ void test_lu_brain_basics_01(void)
 	TEST_ASSERT(lu_wave__get_ix(s_wave) == 0);
 	TEST_ASSERT(lu_brain__get_wave_by_ix(brain, lu_wave__get_ix(s_wave), lu_wave__get_type(s_wave)) == s_wave);
 
-	lu__debug("\n\n ======================== S A V E =========================\n");
+	lu__debug("\n\n ======================== S A V E - 0 =========================\n");
 	lu__debug("\nvalues_0 before push: ");
 
 		lu_values__print_symbols(values[0], 2, 2, 1);
@@ -140,7 +152,7 @@ void test_lu_brain_basics_01(void)
 		label_cell = lu_wave__link_to_label(s_wave, 2, 0, 0, 0, labels[0]);
 		TEST_ASSERT(label_cell);
 
-	lu__debug("\n\n ======================== S A V E - 2 =========================\n");
+	lu__debug("\n\n ======================== S A V E - 1 =========================\n");
 	lu__debug("\nvalues_0 before push: ");
 		lu_rec__set_dest_start_pos(rec_0, 0, 0);
 
@@ -154,6 +166,48 @@ void test_lu_brain_basics_01(void)
 		label_cell = lu_wave__link_to_label(s_wave, 2, 0, 0, 0, labels[1]);
 		TEST_ASSERT(label_cell);
 
+	lu__debug("\n\n ======================== S A V E - 2 =========================\n");
+	lu__debug("\nvalues_0 before push: ");
+		lu_rec__set_dest_start_pos(rec_0, 0, 0);
+
+		lu_wave__push(s_wave, rec_0, values[2], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 1, 1);
+
+		lu_wave__push(s_wave, rec_0, values[2], 2, 2, 1);
+
+		lu_wave__process(s_wave, lu_process_config__get_by_id(LU_PROCESS__SAVE_DEFAULT));
+		label_cell = lu_wave__link_to_label(s_wave, 2, 0, 0, 0, labels[2]);
+		TEST_ASSERT(label_cell);
+
+	lu__debug("\n\n ======================== S A V E - 3 =========================\n");
+	lu__debug("\nvalues_0 before push: ");
+		lu_rec__set_dest_start_pos(rec_0, 0, 0);
+
+		lu_wave__push(s_wave, rec_0, values[3], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 1, 1);
+
+		lu_wave__push(s_wave, rec_0, values[3], 2, 2, 1);
+
+		lu_wave__process(s_wave, lu_process_config__get_by_id(LU_PROCESS__SAVE_DEFAULT));
+		label_cell = lu_wave__link_to_label(s_wave, 2, 0, 0, 0, labels[3]);
+		TEST_ASSERT(label_cell);
+
+	// lu__debug("\n\n ======================== S A V E - 4 =========================\n");
+	// lu__debug("\nvalues_0 before push: ");
+	// 	lu_rec__set_dest_start_pos(rec_0, 0, 0);
+
+	// 	lu_wave__push(s_wave, rec_0, values[4], 2, 2, 1);
+
+	// 	lu_rec__set_dest_start_pos(rec_0, 1, 1);
+
+	// 	lu_wave__push(s_wave, rec_0, values[4], 2, 2, 1);
+
+	// 	lu_wave__process(s_wave, lu_process_config__get_by_id(LU_PROCESS__SAVE_DEFAULT));
+	// 	label_cell = lu_wave__link_to_label(s_wave, 2, 0, 0, 0, labels[4]);
+	// 	TEST_ASSERT(label_cell);
+
 	//
 	// Match
 	//
@@ -163,7 +217,7 @@ void test_lu_brain_basics_01(void)
 	TEST_ASSERT(m_wave);
 	TEST_ASSERT(lu_wave__get_ix(m_wave) == 0);
 	TEST_ASSERT(lu_brain__get_wave_by_ix(brain, lu_wave__get_ix(m_wave), lu_wave__get_type(m_wave)) == m_wave);
-	lu__debug("\n\n ======================== M A T C H =========================\n");
+	lu__debug("\n\n ======================== M A T C H - 0 =========================\n");
 
 		lu__debug("\nvalues_0 before push: ");
 		lu_values__print_symbols(values[0], 2, 2, 1);
@@ -186,7 +240,7 @@ void test_lu_brain_basics_01(void)
 
 		TEST_ASSERT(lu_label__get_id(results[0]) == labels[0]);
 
-	lu__debug("\n\n ======================== M A T C H - 2 =========================\n");
+	lu__debug("\n\n ======================== M A T C H - 1 =========================\n");
 		lu__debug("\nvalues_0 before push: ");
 		lu_values__print_symbols(values[1], 2, 2, 1);
 
@@ -198,14 +252,80 @@ void test_lu_brain_basics_01(void)
 
 		lu_wave__push(m_wave, rec_0, values[1], 2, 2, 1);
 
-	lu_wave__process(m_wave, lu_process_config__get_by_id(LU_PROCESS__MATCH_DIFF_ONLY));
+		lu_wave__process(m_wave, lu_process_config__get_by_id(LU_PROCESS__MATCH_DIFF_ONLY));
 
-	results = lu_wave__get_result_labels(m_wave);
-	TEST_ASSERT(results);
-	TEST_ASSERT(results[0]);
+		results = lu_wave__get_result_labels(m_wave);
+		TEST_ASSERT(results);
+		TEST_ASSERT(results[0]);
 
-	lu_labels__print_results(results, brain_config.w_result_labels_size);
+		lu_labels__print_results(results, brain_config.w_result_labels_size);
 
-	TEST_ASSERT(lu_label__get_id(results[0]) == labels[1]);
+		TEST_ASSERT(lu_label__get_id(results[0]) == labels[1]);
+
+	lu__debug("\n\n ======================== M A T C H - 2 =========================\n");
+		lu__debug("\nvalues_0 before push: ");
+		lu_values__print_symbols(values[2], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 0, 0);
+
+		lu_wave__push(m_wave, rec_0, values[2], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 1, 1);
+
+		lu_wave__push(m_wave, rec_0, values[2], 2, 2, 1);
+
+		lu_wave__process(m_wave, lu_process_config__get_by_id(LU_PROCESS__MATCH_DIFF_ONLY));
+
+		results = lu_wave__get_result_labels(m_wave);
+		TEST_ASSERT(results);
+		TEST_ASSERT(results[0]);
+
+		lu_labels__print_results(results, brain_config.w_result_labels_size);
+
+		TEST_ASSERT(lu_label__get_id(results[0]) == labels[2]);
+
+	lu__debug("\n\n ======================== M A T C H - 3 =========================\n");
+		lu__debug("\nvalues_0 before push: ");
+		lu_values__print_symbols(values[3], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 0, 0);
+
+		lu_wave__push(m_wave, rec_0, values[3], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 1, 1);
+
+		lu_wave__push(m_wave, rec_0, values[3], 2, 2, 1);
+
+		lu_wave__process(m_wave, lu_process_config__get_by_id(LU_PROCESS__MATCH_DIFF_ONLY));
+
+		results = lu_wave__get_result_labels(m_wave);
+		TEST_ASSERT(results);
+		TEST_ASSERT(results[0]);
+
+		lu_labels__print_results(results, brain_config.w_result_labels_size);
+
+		TEST_ASSERT(lu_label__get_id(results[0]) == labels[3]);
+
+	lu__debug("\n\n ======================== M A T C H - 4 =========================\n");
+		lu__debug("\nvalues_0 before push: ");
+		lu_values__print_symbols(values[4], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 0, 0);
+
+		lu_wave__push(m_wave, rec_0, values[4], 2, 2, 1);
+
+		lu_rec__set_dest_start_pos(rec_0, 1, 1);
+
+		lu_wave__push(m_wave, rec_0, values[4], 2, 2, 1);
+
+		lu_wave__process(m_wave, lu_process_config__get_by_id(LU_PROCESS__MATCH_DIFF_ONLY));
+
+		results = lu_wave__get_result_labels(m_wave);
+		TEST_ASSERT(results);
+		TEST_ASSERT(results[0]);
+
+		lu_labels__print_results(results, brain_config.w_result_labels_size);
+
+		TEST_ASSERT(lu_label__get_id(results[0]) == labels[2]);
 
 }
