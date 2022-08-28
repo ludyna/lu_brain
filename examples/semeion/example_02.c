fd8066805e28f41c005311655b9d07276a870da2
Oleh Novosad <oleh.novosad@gmail.com>
2022-08-28 03:02:38 -0400

    example 2 in prog

--- examples/semeion/example_02.c
+++ examples/semeion/example_02.c
@@ -4,190 +4,83 @@
 	WARNING: not working at this moment, WORK IN PROGRESS
 
 	Execute with 
-	$  clear && printf '\e[3J'; rm ./build/semeion; gcc -g -Wall -Wno-unused-function -Wno-unused-value example_02.c -o ./build/semeion -lm; ./build/semeion 
+	$  clear && printf '\e[3J'; rm ./build/semeion; gcc -g -Wall -Wno-unused-function -Wno-unused-value example_01.c -o ./build/semeion -lm; ./build/semeion 
 
 	Flags:
 	-Wno-unused-function - to not show warning about unused functions
 	-Wno-unused-value - to not show warning about unused values
-
+	
 	DESCRIPTION:
 
 */
 
-#include "brain_objects.h"
+#include <stdio.h>
+#include "semeion.h"
+#include "semeion.c"
 
+#define LU_EX__GREETING_SIZE 2000
 
 int main()
 {
-	Lu_Mem_Debugger md = lu_mem_debugger__create(lu_g_mem);
+	char command[120];
+	char token_1[30];
+	char token_2[30];
+	char token_3[30];
+	char greeting[LU_EX__GREETING_SIZE];
 
-	// Radomize rand() with time
+	FILE *fp = fopen("example_02_greeting.txt", "r");
+	if (fp != NULL) {
+	    size_t newLen = fread(greeting, sizeof(char), LU_EX__GREETING_SIZE, fp);
+	    if ( ferror( fp ) != 0 ) {
+	        fputs("Error reading file", stderr);
+	    } else {
+	        greeting[newLen++] = '\0'; /* Just to be safe. */
+	    }
 
-	srand(time(0));
+	    fclose(fp);
+	}
 
-	// Init samples
+	printf("\n%s\n", greeting);
 
-	smn_groups__init();
+	do {
+		
+		//
+		// Reset command and tokens
+		//
+		command[0] = 0;
+		token_1[0] = 0;
+		token_2[0] = 0;
+		token_3[0] = 0;
 
-	// Load data into memory  
+		printf("\ninput> ");
+		fgets(command, 120, stdin);
+		sscanf(command, "%30s %30s %30s", token_1, token_2, token_3);
+		// printf("Tokens: %s, %s, %s", token_1, token_2, token_3);
 
-	smn_data__load();
+		if (strcmp(token_1, "learn") == 0)
+		{
 
-	// Allocate data for groups and fill groups with samples
+		} else if (strcmp(token_1, "match") == 0)
+		{
 
-	smn_groups__data_alloc();
-	smn_groups__data_fill();
+		} else if (strcmp(token_1, "delete") == 0)
+		{
 
-	// Select random samples from each group
+		} else if (strcmp(token_1, "restore") == 0)
+		{
 
-	smn_data__samples_create();
+		} else if (strcmp(token_1, "exit") == 0)
+		{
 
-	// Get brain config 
-	struct lu_config config = lu_config__get_by_id(LU_CONFIG__SEMEION_02);
-	config.w_match_results_size = MATCH_RESULTS_SIZE;
+		}
+		else 
+		{
+			printf("Unknown command.");
+		}
 
-	config.w_match_sig_breakpoint = 0.4;
 
-	focuses__create(config);
+	} while(strcmp(token_1, "exit") != 0);
 
-	// Create brain and related
-
-	// focuses__train_samples();
-
-	// Show random digit
-
-	// printf("\nRandom sample:");
-	// Smn_Digit digit = &smn_data[smn__rand_in_range(0, (int) smn_data_count)];
-	// smn_digit__print(digit);
-
-	// // Training samples
-
-	// size_t i;
-	// Smn_Digit d;
-	
-	// printf("\nTraining %ld samples.. ", smn_training_samples_count);
-
-	// Lu_La_Cell la_cell;
-	// clock_t start;
-	// clock_t end;
-	// start = clock();
-	// for (i = 0; i < smn_training_samples_count; i++)
-	// {
-	// 	d = smn_training_samples[i];
-
-	// 	lu_wave__push(save_wave, image_rec, smn_blank_pixels, 16, 16, 1);
-	// 	lu_wave__push(save_wave, image_rec, d->pixels, 16, 16, 1);
-
-	// 	lu_wave__process(save_wave, lu_save_config__get_by_id(LU_PROCESS__SAVE_DEFAULT));
-
-	// 	la_cell = lu_wave__link_to_label(save_wave, 2, 0, 0, 0, d->name);
-	// 	lu__assert(la_cell);
-
-	// 	//printf("\nTraining samples.. %lu trained.", i + 1);
-	// 	// fflush(stdout);
-	// }
-	// end = clock();
-
-	// // Calculating total time taken 
- //    double time_taken = (double)(end - start) / (double)(CLOCKS_PER_SEC);
-
-	// printf(
-	// 	"\nTraining of %ld samples in a single thread without hardware acceleration took %.1f sec\n", 
-	// 	smn_training_samples_count, 
-	// 	time_taken
-	// );
-
-	// Show network stats
-
-	// focuses__net_stats();
-
-	// focuses__test_samples();
-
-	// // Testing samples
-	
-	// float failed_count 		= 0;
-	// float success_count 	= 0;
-
-	// printf("\nTesting samples.. ");
-
-	// Lu_Label* labels = NULL;
-
-	// lu_size samples_to_test = smn_test_samples_count; 
-	// for (i = 0; i < smn_test_samples_count; i++)
-	// {
-	// 	if (i >= smn_test_samples_count) break;
-
-	// 	// d = smn_test_samples[smn__rand_in_range(0, (int) smn_test_samples_count - 1)]; 
-	// 	d = smn_test_samples[i];
-
-	// 	printf("\nMATCHING UNTRAINED SAMPLE:");
-	// 	smn_digit__print(d);
-
-	// 	lu_wave__push(match_wave, image_rec, smn_blank_pixels, 16, 16, 1);
-	// 	lu_wave__push(match_wave, image_rec, d->pixels, 16, 16, 1);
-
-	// 	lu_wave__process(match_wave, lu_save_config__get_by_id(LU_PROCESS__MATCH_DIFF_ONLY));
-
-	// 	labels = lu_wave__get_result_labels(match_wave);
-
-	// 	if (labels)
-	// 	{
-	// 		if(labels[0])
-	// 		{
-	// 			lu_wave__print_match_results(match_wave);
-
-	// 			if (lu_label__get_id(labels[0]) == d->name)
-	// 			{
-	// 				printf("\nRESULT: %ld (SUCCESS)", lu_label__get_id(labels[0]));
-	// 				++success_count;
-	// 			}
-	// 			else
-	// 			{
-	// 				printf("\nRESULT: %ld (FAILED)", lu_label__get_id(labels[0]));
-	// 				++failed_count;
-	// 			}
-	// 		}
-	// 		else
-	// 		{
-	// 			printf("\nRESULT: no match (FAILED)");
-	// 			++failed_count;
-	// 		}
-	// 	}
-	// 	else
-	// 	{
-	// 		++failed_count;
-	// 	}
-	// }
-
-	// printf("\nTesting samples.. %lu tested.", i);
-	// // fflush(stdout);
-	// printf("\n");
-
-	// printf("\nReport:");
-	// printf("\n 	  Successfully recognized: %d", (int) success_count);
-	// printf("\n 	  Failed recognition: %d", (int) failed_count);
-	// printf("\n 	  Accuracy rate: %.2f%%", (success_count / (lu_value) samples_to_test) * 100.0);
-	// printf("\n");
-
-	// Destroy save_wave and brain
-
-	// lu_wave__destroy(match_wave);
-	// lu_wave__destroy(save_wave);
-	// lu_brain__destroy(brain_tl);
-
-	focuses__destroy();
-
-	// Clean up memory for data
-
-	smn_data__samples_free();
-	smn_groups__data_free();
-	smn_data__free();
-
-	// Exit app
-
-	lu_mem_debugger__print(md);
-	lu_mem_debugger__destroy(md, true);
-
-	exit(EXIT_SUCCESS);
+	printf("\n");
 }
 
